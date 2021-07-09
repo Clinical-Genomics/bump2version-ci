33dd9c199352379168b18324f3e247bc533d42a6
Mropat <mariaropat@gmail.com>
2021-07-09 18:00:35 +0200

    self commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ git clone "${REPOSITORY}"
 cd "$(ls)" || exit
 
 bump2version --config-file .bumpversion.cfg "${VERSION}"
+git add .
+git commit https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git -m "Automated version bump"
 git push https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git push https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
 
