550db7fc62e65dbfc78707de454c0fc1e0f61e6f
Mropat <mariaropat@gmail.com>
2021-07-09 17:14:32 +0200

    remove arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -o pipefail
 
-REPOSITORY=$1
-REPOSITORY="https://github.com/Clinical-Genomics/bump2version-ci.git"
+#REPOSITORY=$1
+#REPOSITORY="https://github.com/Clinical-Genomics/bump2version-ci.git"
 VERSION="patch"
-REPOSITORY_NAME=$(echo "${REPOSITORY}"|rev|cut -f 1 -d '/'|rev|cut -d '.' -f 1)
+REPOSITORY_NAME=$(echo "${INPUT_REPOSITORY}"|rev|cut -f 1 -d '/'|rev|cut -d '.' -f 1)
 
 
 git clone "${REPOSITORY}"
