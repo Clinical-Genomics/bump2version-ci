d272f68d00869771997c235e32b9cf6e33b7c61b
Mropat <mariaropat@gmail.com>
2021-07-09 20:19:46 +0200

    tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ VERSION="patch"
 git clone "${REPOSITORY}"
 cd "$(ls)" || exit
 
-bump2version --config-file .bumpversion.cfg "${VERSION}"
+#bump2version --config-file .bumpversion.cfg "${VERSION}"
+git tag "tag"
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
 
