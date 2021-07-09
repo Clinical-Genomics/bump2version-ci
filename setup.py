c01e1180b1fb8b403fce3bed8ddbba9a03dc9318
Mropat <mariaropat@gmail.com>
2021-07-09 16:17:07 +0200

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ NAME = "TEST"
 
 setup(
     name=NAME,
-    version="0.0.0",
+    version="0.0.1",
     description=DESCRIPTION,
     long_description="TEST",
     long_description_content_type="text/markdown",
