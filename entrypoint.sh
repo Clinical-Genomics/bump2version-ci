62cec023fe20adb9ed04fe7ffd82fb68c2d94aed
Mropat <mariaropat@gmail.com>
2021-07-09 20:01:22 +0200

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ cd "$(ls)" || exit
 
 bump2version --config-file .bumpversion.cfg "${VERSION}"
 git add .
-git commit https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git -m "Automated version bump"
-git push https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
-git push https://${GIHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
+git commit https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git -m "Automated version bump"
+git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
 
 
 
