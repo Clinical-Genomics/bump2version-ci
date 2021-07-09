1f96b7bb725ff6c941f6a77e8375fb198cb9c872
Mropat <mariaropat@gmail.com>
2021-07-09 20:36:32 +0200

    remove verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-set -eox pipefail
+set -eo pipefail
 
 REPOSITORY="https://github.com/${GITHUB_REPOSITORY}.git"
-echo "Cloning ${REPOSITORY}"
 VERSION="patch"
 
 git clone "${REPOSITORY}"
 cd "$(ls)" || exit
 
 git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.name "Github CI"
 bump2version --config-file .bumpversion.cfg "${VERSION}"
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
