c76bb9ac169f55393ce8df9854450e8b8606c2b2
Mropat <mariaropat@gmail.com>
2021-07-11 21:45:34 +0200

    pass author

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,7 @@ git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
 git pull
 
+BUMPVERSION_AUTHOR=$INPUT_RELEASE-AUTHOR
 BUMPVERSION_TOKEN=$INPUT_RELEASE-PAT
 NEW_TAG="$(git describe)"
 POST_DATA=$(echo {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false})
@@ -32,7 +33,7 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -d "$POST_DATA" \
-  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$BUMPVERSION_TOKEN"
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$BUMPVERSION_AUTHOR:$BUMPVERSION_TOKEN"
 
 
 
