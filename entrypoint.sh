1183ef04e45865ca6642b86452e7fafa8c208117
mayabrandi <maya.brandi@scilifelab.se>
2021-07-12 09:44:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ REPOSITORY="https://github.com/${GITHUB_REPOSITORY}.git"
 git clone "${REPOSITORY}"
 cd "$(ls)" || exit
 
+# Fetching the commit messages for the current branch??
 COMMIT_MSG=$(git log -1 --pretty=%B)
 
+# Parsing patch
 if [[ $COMMIT_MSG == *'major'* ]]; then
   VERSION='major';
 elif [[ $COMMIT_MSG == *'minor'* ]]; then
@@ -17,6 +19,10 @@ else
   VERSION='patch';
 fi
 
+# What is this? 41898282+github-actions[bot]@users.noreply.github.com
+# Configuring ... wat?
+# Bumping and pushing
+# Why pulling?
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "Github CI"
 bump2version --config-file .bumpversion.cfg "${VERSION}"
@@ -24,7 +30,8 @@ git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
 git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
 git pull
 
-NEW_TAG="$(git describe)"
+# Fetching new tag ?? and releasing it using the PAT of whom? 
+NEW_TAG="$(git describe)" 
 POST_DATA=$(echo {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false})
 echo "Submitting release for $NEW_TAG"
 curl \
