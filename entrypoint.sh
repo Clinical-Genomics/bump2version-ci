20073687e46b0c35a2f322a9d364b5762be2e12c
Mropat <mariaropat@gmail.com>
2021-07-13 18:05:18 +0200

    add commit body to release

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,57 @@
 #!/bin/bash
 
-# Requiered env vars:
+## Required env vars:
 
-# GITHUB_TOKEN: 
-# GITHUB_REPOSITORY: 
-# GITHUB_ACTOR: 
+# GITHUB_TOKEN: github-actions workflow native token
+# GITHUB_ACTOR: github-actions workflow native user (github-actions[bot])
+# GITHUB_REPOSITORY: repository this action runs on
 
-# INPUT_RELEASE_PAT:
+# INPUT_RELEASE_PAT: Personal Access Token of the github user who will author the release
 
-
-
-
+# Ensure that when any step of this script fails, the entire job will fail as well
 set -eo pipefail
 
-REPOSITORY="https://github.com/${GITHUB_REPOSITORY}.git"
+REPOSITORY_URI="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git clone "${REPOSITORY}"
+git clone "${REPOSITORY_URI}"
+
 cd "$(ls)" || exit
 
-# Fetching the commit messages for the current branch??
+# Fetching the commit message for the latest commit to branch this action is applied to
 COMMIT_MSG=$(git log -1 --pretty=%B)
 
-# Parsing patch
+# Parsing version update increment from commit message
 if [[ $COMMIT_MSG == *'major'* ]]; then
-  VERSION='major';
+  VERSION='major'
 elif [[ $COMMIT_MSG == *'minor'* ]]; then
-  VERSION='minor';
+  VERSION='minor'
 else
-  VERSION='patch';
+  VERSION='patch'
 fi
 
-# What is this? 41898282+github-actions[bot]@users.noreply.github.com
-# Configuring ... wat?
-# Bumping and pushing
-# Why pulling?
+# Configure email of the github user to the one of the github-actions bot
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "Github CI"
+
+# Run bump2version
 bump2version --config-file .bumpversion.cfg "${VERSION}"
-git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
-git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git --tags
+git push "${REPOSITORY_URI}"
+git push "${REPOSITORY_URI}" --tags
+
+# Pull from branch again to update latest tag
 git pull
 
-# Fetching new tag ?? and releasing it using the PAT of whom? 
-NEW_TAG="$(git describe)" 
-POST_DATA=$(echo {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false})
-echo "Submitting release for $NEW_TAG"
+# Get the value of latest tag that was just pushed
+NEW_TAG="$(git describe)"
+
+# Construct post JSON for publishing release
+POST_DATA=$(echo {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false, \"body\": $COMMIT_MSG})
+
+echo "Submitting release for ${NEW_TAG}"
+
 curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $INPUT_RELEASE_PAT" \
+  -H "Authorization: token ${INPUT_RELEASE_PAT}" \
   -d "$POST_DATA" \
   "https://api.github.com/$GITHUB_REPOSITORY/releases"
-
-
-
-
-
-
