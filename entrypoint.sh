f99d0a00ce6becfc685b64563e4f71e6359b18fe
Mropat <mariaropat@gmail.com>
2021-08-06 15:57:35 +0200

    use organisation bot instead of gh action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,36 +2,33 @@
 
 ## Required env vars:
 
-# GITHUB_TOKEN: github-actions workflow native token
-# GITHUB_ACTOR: github-actions workflow native user (github-actions[bot])
+# BUMPVERSION_TOKEN: personal access token
+# BUMPVERSION_AUTHOR: personal access token author
 # GITHUB_REPOSITORY: repository this action runs on
 
-# INPUT_RELEASE_PAT: Personal Access Token of the github user who will author the release
 
 # Ensure that when any step of this script fails, the entire job will fail as well
 set -eo pipefail
 
-REPOSITORY_URI="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+REPOSITORY_URI="https://${BUMPVERSION_AUTHOR}:${BUMPVERSION_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git clone "${REPOSITORY_URI}"
 
 cd "$(ls)" || exit
 
+git remote add origin "${REPOSITORY_URI}"
+
 # Fetching the commit message for the latest commit to branch this action is applied to
 COMMIT_MSG=$(git log -1 --pretty=%B|sed 's/\r$//g'|sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\\\n/g')
 
 # Parsing version update increment from commit message
-if [[ $COMMIT_MSG == *'major'* ]]; then
+if [[ $COMMIT_MSG == *'(major)'* ]]; then
   VERSION='major'
-elif [[ $COMMIT_MSG == *'minor'* ]]; then
+elif [[ $COMMIT_MSG == *'(minor)'* ]]; then
   VERSION='minor'
 else
   VERSION='patch'
 fi
-
-# Configure email of the github user to the one of the github-actions bot
-git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --global user.name "Github CI"
 
 # Run bump2version
 bump2version --config-file .bumpversion.cfg "${VERSION}"
@@ -52,7 +49,7 @@ echo "Submitting release for ${NEW_TAG}"
 
 curl \
   -X POST \
-  -H "Authorization: token ${INPUT_RELEASE_PAT}" \
+  -H "Authorization: token ${BUMPVERSION_TOKEN}" \
   -d "${POST_DATA}" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
