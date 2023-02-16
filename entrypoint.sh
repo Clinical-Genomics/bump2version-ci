d43913717e23047472d1fe3e80694e66e1440c85
seallard <seallards@gmail.com>
2023-02-16 15:28:25 +0100

    Fix commit message parse error

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ git pull
 NEW_TAG="$(git describe)"
 
 # Construct post JSON for publishing release
-POST_DATA=$(echo -e {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false, \"body\": \""${COMMIT_MSG}"\"})
+POST_DATA=$(echo -e {\"tag_name\": \"$NEW_TAG\", \"name\": \"Release $NEW_TAG\", \"draft\": false, \"prerelease\": false, \"body\": \"${COMMIT_MSG}\"\})
 
 echo "Submitting release for ${NEW_TAG}"
 
